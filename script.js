8648dbf21b328ade6c1954a492d29d63ccf2430a
KurumiRockbell <Kurumi.Rockbell@proton.me>
2022-11-29 13:11:09 +0100

    ajout des fichiers au dépôt

---
+++ script.js
@@ -0,0 +1,36 @@
+/* Fonction pour clear et remettre a 0 */
+
+function forclear() {
+    document.getElementById("output").innerHTML = "0";
+}
+
+
+/* Fonction pour ne pas afficher le zero des le debut 0 */
+function removeZero() {
+    var value = document.getElementById("output").innerHTML;
+    if (value == "0") {
+         value = " "
+         document.getElementById("output").innerHTML = value;
+    }
+}
+
+/* Fonction pour diviser par 100 en cas de division */
+function perc() {
+    var value = document.getElementById("output").innerHTML;
+    value = value / 100;
+    document.getElementById("output").innerHTML = value;
+}
+
+/* Fonction pour afficher le chiffre sur lequel nous avons appuyer */
+function fordisplay(value) {
+    removeZero()
+    document.getElementById("output").innerHTML += value;
+}
+
+/* Fonction pour calculer et afficher le resultat */
+function solve() {
+    removeZero()
+    var equation = document.getElementById("output").innerHTML;
+    var solved = eval(equation);
+    document.getElementById('output').innerHTML = solved;
+}
